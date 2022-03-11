839d7278312475ffe1fe6fe6b6df2ccca6e8ffcf
Chad Mills <cmills@outlook.com>
2022-03-11 01:25:17 -0600

    added back accidentally missing invite fields in invite summary view

--- db/Procedures/InviteWithLocalStatus.sql
+++ db/Procedures/InviteWithLocalStatus.sql
@@ -1,6 +1,15 @@
 create view InviteWithLocalStatus as
 select 
+	RefugeeId,
 	HostId, 
+	Message,
+	DateSent,
+	RefugeeRescinded,
+	HostRescinded,
+	ExpirationDate,
+	AcceptedDate,
+	DateToClose,
+	CompletedDate,
 	Archived,
 	dbo.InviteIsLive(DateSent, RefugeeRescinded, HostRescinded, ExpirationDate, AcceptedDate, DateToClose, CompletedDate, Archived) as IsLive,
 	dbo.InviteIsActive(DateSent, RefugeeRescinded, HostRescinded, ExpirationDate, AcceptedDate, DateToClose, CompletedDate, Archived) as IsActive,
